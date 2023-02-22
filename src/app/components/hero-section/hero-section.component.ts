cb3a02ce409f1d86b89c475a362c893664aa51ab
nyesoviktoria <nyeso.viktoria@gmail.com>
2023-02-22 18:31:02 +0100

    feat: BVKZ-16 basic hero component carousel built

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IMAGES_ROUTE } from '../../constants/app.constants';
+import { MatCarousel, MatCarouselComponent, MatCarouselSlide, MatCarouselSlideComponent } from '@magloft/material-carousel';
 
 @Component({
   selector: 'bvkz-hero-section',
