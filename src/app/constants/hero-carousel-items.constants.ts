8f72f63f3f5fda27c0356c012cb7166f9a1249cf
nyesoviktoria <nyeso.viktoria@gmail.com>
2023-02-24 16:44:05 +0100

    carousel slide picture changed, scroll-to-top added upon navigation

--- src/app/constants/hero-carousel-items.constants.ts
+++ src/app/constants/hero-carousel-items.constants.ts
@@ -3,9 +3,9 @@ import { CONCERTS_PATH, GALLERY_PATH } from './routing.constants';
 
 export const HERO_CAROUSEL_ITEMS: readonly HeroCarouselItem[] = [
   {
-    imageMobileSource: 'slide2-mobile.webp',
-    imageTabletSource: 'slide2.webp',
-    imageDesktopSource: 'slide2-full.webp',
+    imageMobileSource: 'slide1-mobile.webp',
+    imageTabletSource: 'slide1-mobile.webp',
+    imageDesktopSource: 'slide1-desktop.webp',
     slideTitle: 'BVKZ.HERO__COMPONENT.HERO_1__TITLE',
     slideText: 'BVKZ.HERO__COMPONENT.HERO_1__TEXT',
     navigateTo: CONCERTS_PATH,
@@ -13,8 +13,8 @@ export const HERO_CAROUSEL_ITEMS: readonly HeroCarouselItem[] = [
   },
   {
     imageMobileSource: 'slide2-mobile.webp',
-    imageTabletSource: 'slide2.webp',
-    imageDesktopSource: 'slide2-full.webp',
+    imageTabletSource: 'slide2-tablet.webp',
+    imageDesktopSource: 'slide2-desktop.webp',
     slideTitle: 'BVKZ.HERO__COMPONENT.HERO_2__TITLE',
     slideText: 'BVKZ.HERO__COMPONENT.HERO_2__TEXT',
     navigateTo: GALLERY_PATH,
