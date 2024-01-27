68f9ca7b11d142a9c6c5abc9ed96c9f1d4f63139
nyesoviktoria <nyeso.viktoria@gmail.com>
2024-01-27 09:17:17 +0100

    new concerts is added. hero section is updated

--- src/app/constants/hero-carousel-items.constants.ts
+++ src/app/constants/hero-carousel-items.constants.ts
@@ -3,9 +3,9 @@ import { CONCERTS_PATH, GALLERY_PATH } from './routing.constants';
 
 export const HERO_CAROUSEL_ITEMS: readonly HeroCarouselItem[] = [
   {
-    imageMobileSource: 'slide1-mobile',
-    imageTabletSource: 'slide1-tablet',
-    imageDesktopSource: 'slide1-desktop',
+    imageMobileSource: 'slide1-mobile-godollo',
+    imageTabletSource: 'slide1-tablet-godollo',
+    imageDesktopSource: 'slide1-desktop-godollo',
     imageAltTranslationKey: 'BVKZ.HERO__COMPONENT.HERO_1__IMAGE_ALT_TEXT',
     slideTitle: 'BVKZ.HERO__COMPONENT.HERO_1__TITLE',
     slideText: 'BVKZ.HERO__COMPONENT.HERO_1__TEXT',
